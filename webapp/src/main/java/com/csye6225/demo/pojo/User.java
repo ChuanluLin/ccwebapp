60e1ab9856a28739a1d4c07f1a91ea6c3118d79d
ShangruiXie <xie.sha@husky.neu.edu>
2019-09-24 01:06:35 -0400

    delete unused import code

--- webapp/src/main/java/com/csye6225/demo/pojo/User.java
+++ webapp/src/main/java/com/csye6225/demo/pojo/User.java
@@ -1,6 +1,5 @@
 package com.csye6225.demo.pojo;
 
-import com.csye6225.demo.validation.ValidPassword;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.GenericGenerator;
 
