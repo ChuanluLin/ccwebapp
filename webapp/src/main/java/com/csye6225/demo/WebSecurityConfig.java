39afa24d487423f574fbd6162af2c163551625dc
Tianli Feng <feng.tian@husky.neu.edu>
2019-09-24 02:38:09 -0400

    Assignment2_security config

---
+++ webapp/src/main/java/com/csye6225/demo/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.csye6225.demo;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select email as username, password, 1 as enabled"
+                        + " from user where email=?")
+                .authoritiesByUsernameQuery("select email as username, 'USER' as authority "
+                        + "from user where email=?")
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // it indicate basic authentication is requires
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                // index will be accessible directly, no need of any authentication
+                .antMatchers( "/v1/user").permitAll()
+                // it's indicate all request will be secure, actually for only one user, authenticated() is enough
+                .anyRequest().hasAuthority("USER");
+        http.
+                csrf().disable()
+                // don't create session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
