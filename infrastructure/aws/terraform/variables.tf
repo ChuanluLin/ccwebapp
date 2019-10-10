d46c8abc04d3efea57a6c8542d3d8c93e644f2f0
ShangruiXie <xie.sha@husky.neu.edu>
2019-10-10 10:34:48 -0700

    set previous version

--- infrastructure/aws/terraform/variables.tf
+++ infrastructure/aws/terraform/variables.tf
@@ -14,30 +14,10 @@ variable "vpc_cidr_block" {
   description = "The VPC CIDR block (x.x.x.x/x)"
 }
 
-variable "subnet_count" {
-  default = "3"
+variable "subnet_cidr_blocks" {
+  description = "A comma-delimited list of CIDR block for the Subnets (x.x.x.x/x)"
 }
 
-variable "subnet1_cidr_block" {
-  description = "The CIDR block for the 1st Subnet (x.x.x.x/x)"
-}
-
-variable "subnet2_cidr_block" {
-  description = "The CIDR block for the 2nd Subnet (x.x.x.x/x)"
-}
-
-variable "subnet3_cidr_block" {
-  description = "The CIDR block for the 3rd Subnet (x.x.x.x/x)"
-}
-
-variable "availability_zone1" {
-  description = "The availability zone for the 1st Subnet."
-}
-
-variable "availability_zone2" {
-  description = "The availability zone for the 2nd Subnet."
-}
-
-variable "availability_zone3" {
-  description = "The availability zone for the 3rd Subnet."
+variable "availability_zones" {
+  description = "A comma-delimited list of availability zones for the VPC."
 }
