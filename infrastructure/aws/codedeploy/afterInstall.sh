787ef7ebe08d7c85e2d54e834220c313f877f255
Tianli Feng <feng.tian@husky.neu.edu>
2019-11-01 02:58:54 -0400

    A7 nohup

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+# stop application
 # sudo systemctl stop tomcat.service
-
 # sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
-
 # sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
-
 ps aux | grep demo | xargs kill -9
-nohup java -jar ~/demo-0.0.1-SNAPSHOT.jar >spring.log 2>&1 &
 
 # cleanup log files
 # sudo rm -rf /opt/tomcat/logs/catalina*
@@ -15,3 +12,6 @@ nohup java -jar ~/demo-0.0.1-SNAPSHOT.jar >spring.log 2>&1 &
 # sudo rm -rf /opt/tomcat/logs/*.txt
 rm -rf /home/centos/spring.log
 rm -rf /home/centos/access_log.log
+
+# start application
+nohup java -jar ~/demo-0.0.1-SNAPSHOT.jar >spring.log 2>&1 &
