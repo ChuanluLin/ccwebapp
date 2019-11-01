180528720212c9cef0f17ecf3082c60262b0edfe
Tianli Feng <feng.tian@husky.neu.edu>
2019-11-01 02:31:23 -0400

    A7 nohup

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -7,7 +7,7 @@
 # sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 
 ps aux | grep demo | xargs kill -9
-nohup java -jar ~/demo-0.0.1-SNAPSHOT.jar 2>&1 &
+nohup java -jar ~/demo-0.0.1-SNAPSHOT.jar >/dev/null 2>&1
 
 # cleanup log files
 # sudo rm -rf /opt/tomcat/logs/catalina*
